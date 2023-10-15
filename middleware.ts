3ae8a2c21063b46ae5367b5d7743d30990ec107c
Rossen Roussev <rossenroussev@MacBook-Pro-16.local>
2023-10-15 20:31:51 +0200

    Catch errors in the middleware

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,27 @@ import { UserBusiness, UserRole } from "types";
 import { BACKEND_URL, DataCollections } from "utils";
 
 export async function middleware(request: NextRequest) {
+  console.log("middleware");
   const pbClient = new PocketBase(BACKEND_URL);
 
   pbClient.authStore.loadFromCookie(request.headers.get("cookie") ?? "");
 
   const userId = pbClient.authStore.model?.id;
 
+  console.log("auth store", pbClient.authStore);
   if (pbClient.authStore.isValid) {
-    const userRoles: UserRole[] = await pbClient
-      .collection(DataCollections.USER_ROLES)
-      .getFullList(1, {
-        filter: `user="${userId}"&&(role.name="ADMIN"||role.name="SUPER ADMIN")`,
-      });
-    // If the user is Admin - allow him to continue without anymore checks
-    if (userRoles.length) {
-      return NextResponse.next();
+    try {
+      const userRoles: UserRole[] = await pbClient
+        .collection(DataCollections.USER_ROLES)
+        .getFullList(1, {
+          filter: `user="${userId}"&&(role.name="ADMIN"||role.name="SUPER ADMIN")`,
+        });
+      // If the user is Admin - allow him to continue without anymore checks
+      if (userRoles.length) {
+        return NextResponse.next();
+      }
+    } catch (error) {
+      console.log("Error getting user roles", error);
     }
   }
 
@@ -34,20 +40,26 @@ export async function middleware(request: NextRequest) {
     }
 
     const businessId = spltPathname[4];
+    const businessResponse = NextResponse.redirect(
+      new URL(`/businesses/${businessId}`, request.url)
+    );
     // Check if the current user has the required permissions to edit this business
-    const permissions: UserBusiness[] = await pbClient
-      .collection(DataCollections.USER_BUSINESSES)
-      .getFullList(1, {
-        filter: `user="${userId}"&&business="${businessId}"`,
-      });
+    try {
+      const permissions: UserBusiness[] = await pbClient
+        .collection(DataCollections.USER_BUSINESSES)
+        .getFullList(1, {
+          filter: `user="${userId}"&&business="${businessId}"`,
+        });
 
-    if (!permissions.length) {
-      return NextResponse.redirect(
-        new URL(`/businesses/${businessId}`, request.url)
-      );
+      if (!permissions.length) {
+        return businessResponse;
+      }
+    } catch (error) {
+      return businessResponse;
     }
   }
 
+  console.log("next response");
   return NextResponse.next();
 }
 
