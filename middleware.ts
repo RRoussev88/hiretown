553ad96bf22129112604e85ed4bef58513652a8b
Rossen Roussev <rroussev88@hotmail.com>
2023-11-01 04:28:14 +0100

    Removed requests from the middleware

--- middleware.ts
+++ middleware.ts
@@ -9,78 +9,12 @@ export async function middleware(request: NextRequest) {
 
   pbClient.authStore.loadFromCookie(request.headers.get("cookie") ?? "");
 
-  const userId = pbClient.authStore.model?.id;
-
-  const requestInit = {
-    headers: {
-      Authorization: pbClient.authStore.token,
-      // "Content-Type": "application/json",
-    },
-  };
-  console.log("requestInit: ", requestInit);
-
   if (pbClient.authStore.isValid) {
-    try {
-      const url = new URL(`${BACKEND_URL}/api/collections/userRoles/records`);
-      url.searchParams.set(
-        "filter",
-        `(user="${userId}" && (role.name="ADMIN"||role.name="SUPER ADMIN"))`
-      );
-      console.log("URL: ", url.toString());
-      const res = await fetch(url, requestInit);
-      console.log("roles res: ", res);
-      const data = await res.json();
-      console.log("roles data: ", data);
-
-      // If the user is Admin - allow him to continue without anymore checks
-      if (data.items?.length) {
-        return NextResponse.next();
-      }
-    } catch (error) {
-      console.log(`USER_ROLES Error ${error}`);
-    }
+    return NextResponse.next();
   } else if (request.nextUrl.pathname.startsWith("/profile/")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  const spltPathname = request.nextUrl.pathname.split("/");
-  if (
-    spltPathname.length > 4 &&
-    request.nextUrl.pathname.startsWith("/profile/activity/businesses/")
-  ) {
-    const businessId = spltPathname[4];
-    const businessResponse = NextResponse.redirect(
-      new URL(`/businesses/${businessId}`, request.url)
-    );
-    console.log("user: ", userId);
-    console.log("businessId: ", businessId);
-
-    try {
-      // Check if the current user has the required permissions to edit this business
-      const url = new URL(
-        `${BACKEND_URL}/api/collections/userBusinesses/records`
-      );
-      url.searchParams.set(
-        "filter",
-        `(user="${userId}" && business="${businessId}")`
-      );
-      console.log("URL: ", url.toString());
-      const res = await fetch(url, requestInit);
-      console.log("business res: ", res);
-      const data = await res.json();
-      console.log("busiesses data: ", data);
-
-      if (!data.items?.length) {
-        console.log("Second /businesses/:id redirect");
-        return businessResponse;
-      }
-    } catch (error) {
-      console.log(`USER_BUSINESSES Error ${error}`);
-      console.log(`Redirect /businesses/${businessId} userId=${userId}`);
-      return businessResponse;
-    }
-  }
-  console.log("Last next()");
   return NextResponse.next();
 }
 
